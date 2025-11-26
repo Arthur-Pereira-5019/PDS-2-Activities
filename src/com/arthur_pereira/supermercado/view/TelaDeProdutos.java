3a1f538ae8feeb7b28c767ce65695bf6cb6e5587
Arthur <arthur.p31@aluno.ifsc.edu.br>
2025-11-26 08:49:50 -0300

    Fixed responsiviness in products screen

--- src/com/arthur_pereira/supermercado/view/TelaDeProdutos.java
+++ src/com/arthur_pereira/supermercado/view/TelaDeProdutos.java
@@ -21,23 +21,23 @@ public class TelaDeProdutos extends TelaAbstrata {
 	Popups popups = new Popups();
 	public TelaDeProdutos() {
 		super(360,360);
-		getContentPane().setLayout(new MigLayout("", "[grow][111,grow][grow][111,grow][grow][111,grow][grow]", "[grow][grow][grow][grow][grow][grow][grow][grow]"));
+		getContentPane().setLayout(new MigLayout("", "[grow 15][111,grow][grow][111,grow][grow][111,grow][grow 15]", "[grow 15][][grow 15][][][][grow 35][][][][][][grow 15]"));
 		getContentPane().setBackground(backgroundC);
 
 		
 		JLabel lblNewLabel = new JLabel("Produtos");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 28));
-		getContentPane().add(lblNewLabel, "cell 3 0,alignx center");
+		getContentPane().add(lblNewLabel, "cell 3 1,alignx center");
 		lblNewLabel.setForeground(textC);
 		
 		JLabel lblNewLabel_1 = new JLabel("Id do Produto");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		getContentPane().add(lblNewLabel_1, "cell 1 1,alignx center");
+		getContentPane().add(lblNewLabel_1, "cell 1 3,alignx center");
 		lblNewLabel_1.setForeground(textC);
 
 		
 		campoId = new JTextField();
-		getContentPane().add(campoId, "cell 1 2 6 1,growx");
+		getContentPane().add(campoId, "cell 1 5 6 1,growx");
 		campoId.setColumns(10);
 		campoId.setBackground(highlightC);
 		campoId.setForeground(textC);
@@ -45,34 +45,34 @@ public class TelaDeProdutos extends TelaAbstrata {
 		
 		JButton btnNewButton = new JButton("Encontrar");
 		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		getContentPane().add(btnNewButton, "cell 2 3");
+		getContentPane().add(btnNewButton, "cell 2 7");
 		btnNewButton.setBackground(highlightC);
 		btnNewButton.setForeground(textC);
 		
 		JButton btnNewButton_1 = new JButton("Excluir");
 		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		getContentPane().add(btnNewButton_1, "cell 5 3,growx");
+		getContentPane().add(btnNewButton_1, "cell 5 7,growx");
 		btnNewButton_1.setBackground(highlightC);
 		btnNewButton_1.setForeground(textC);
 		
 		JLabel lblNewLabel_2 = new JLabel("Nome");
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		getContentPane().add(lblNewLabel_2, "cell 1 4,alignx center");
+		getContentPane().add(lblNewLabel_2, "cell 1 9,alignx center");
 		lblNewLabel_2.setForeground(textC);
 		
 		JLabel lblNewLabel_3 = new JLabel("Quantidade");
 		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		getContentPane().add(lblNewLabel_3, "cell 3 4,alignx center");
+		getContentPane().add(lblNewLabel_3, "cell 3 9,alignx center");
 		lblNewLabel_3.setForeground(textC);
 		
 		JLabel lblNewLabel_4 = new JLabel("Preço");
 		lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		getContentPane().add(lblNewLabel_4, "cell 5 4,alignx center");
+		getContentPane().add(lblNewLabel_4, "cell 5 9,alignx center");
 		lblNewLabel_4.setForeground(textC);
 
 		
 		campoNome = new JTextField();
-		getContentPane().add(campoNome, "cell 1 5,alignx center");
+		getContentPane().add(campoNome, "cell 1 10,alignx center");
 		campoNome.setColumns(10);
 		campoNome.setBackground(highlightC);
 		campoNome.setForeground(textC);
@@ -80,26 +80,26 @@ public class TelaDeProdutos extends TelaAbstrata {
 		
 		campoPreco = new JTextField();
 		campoPreco.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		getContentPane().add(campoPreco, "cell 3 5,growx");
+		getContentPane().add(campoPreco, "cell 3 10,growx");
 		campoPreco.setColumns(10);
 		campoPreco.setBackground(highlightC);
 		campoPreco.setForeground(textC);
 		
 		campoQuantidade = new JTextField();
-		getContentPane().add(campoQuantidade, "cell 5 5,growx");
+		getContentPane().add(campoQuantidade, "cell 5 10,growx");
 		campoQuantidade.setColumns(10);
 		campoQuantidade.setBackground(highlightC);
 		campoQuantidade.setForeground(textC);
 		
 		JButton btnNewButton_2 = new JButton("Criar");
 		btnNewButton_2.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		getContentPane().add(btnNewButton_2, "cell 2 6,growx");
+		getContentPane().add(btnNewButton_2, "cell 2 11,growx");
 		btnNewButton_2.setBackground(highlightC);
 		btnNewButton_2.setForeground(textC);
 		
 		JButton btnNewButton_3 = new JButton("Atualizar");
 		btnNewButton_3.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		getContentPane().add(btnNewButton_3, "cell 5 6,alignx center");
+		getContentPane().add(btnNewButton_3, "cell 4 11,growx");
 		btnNewButton_3.setBackground(highlightC);
 		btnNewButton_3.setForeground(textC);
 	}
